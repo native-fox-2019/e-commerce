c67b086ba43212e4840de8eb3eda5ffe17e3659a
Singgit Bramantha <bambadom@gmail.com>
2020-03-16 15:01:03 +0700

    buat login register controller dan seeder admin dan migration

--- server/ecommerce.test.js
+++ server/ecommerce.test.js
@@ -11,5 +11,27 @@ afterAll(done => {
 });
 
 describe('Create a product item', function(){
-    describe('Successfully create product')
+    describe('Successfully create product', function() {
+        test('Should return 201 and object (message, product)', (done) =>{
+            request(app)
+            .post('/products')
+            .send({
+                name: 'jam tangan',
+                image_url:`https://jamtangan123`,
+                price:1000000,
+                stock:30
+            })
+            .then(result =>{
+                const { body, status } = result
+                expect(status).toBe(201)
+                expect(body).toHaveProperty('message', 'Successfully create product')
+                expect(body).toHaveProperty('product')
+                expect(body.product.name).toBe('jam tangan')
+                expect(body.product.image_url).toBe('https://jamtangan123')
+                expect(body.product.price).toBe(1000000)
+                expect(body.product.stock).toBe(30)
+                done()
+            })
+        })
+    })
 })
