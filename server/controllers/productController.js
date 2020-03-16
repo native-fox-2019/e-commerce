11b6b5410359c2c281b9517c320665b7273d5c01
Singgit Bramantha <bambadom@gmail.com>
2020-03-16 18:19:42 +0700

    masih membetulkan error di app.test.js

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,35 +1,33 @@
 
-const { Product } = require('../models')
-require('dotenv').config()
+// const { Product } = require('../models')
+// require('dotenv').config()
 
-class ProductController {
+// class ProductController {
+//     static addNewProduct = (req, res) => {
+//         let obj = {
+//             name: req.body.name,
+//             image_url: req.body.image_url,
+//             price: req.body.price,
+//             stock: req.body.stock
+//         }
+//         Product.create(obj)
+//             .then(data => {
+//                 res.status(201).json(data)
+//             })
+//             .catch(err => {
+//                 if (err) {
+//                     let obj = {
+//                         error: []
+//                     }
+//                     for (let i = 0; i < err.errors.length; i++) {
+//                         obj.error.push(err.errors[i].message)
+//                     }
+//                     res.status(400).json(obj)
+//                 } else {
+//                     res.status(500).json(err)
+//                 }
+//             })
+//     }
+// }
 
-    static addProduct = (req, res) => {
-
-        let obj = {
-            name: req.body.name,
-            image_url: req.body.image_url,
-            price: req.body.price,
-            stock: req.body.stock
-        }
-        Product.create(obj)
-            .then(data => {
-                res.status(201).json(data)
-            })
-            .catch(err => {
-                if (err) {
-                    let obj = {
-                        error: []
-                    }
-                    for (let i = 0; i < err.errors.length; i++) {
-                        obj.error.push(err.errors[i].message)
-                    }
-                    res.status(400).json(obj)
-                } else {
-                    res.status(500).json(err)
-                }
-            })
-    }
-}
-
-module.exports = ProductController
+// module.exports = ProductController
--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -1,10 +1,10 @@
-const router = require('express').Router()
-const ProductController = require('../controllers/productController')
+// const router = require('express').Router()
+// const ProductController = require('../controllers/productController')
 
 
-router.post('/',  ProductController.addProduct)
+// router.post('/',  ProductController.addNewProduct)
 // router.get('/',  TaskController.getProduct)
 // router.put('/:id',  TaskController.editProduct)
 // router.delete('/:id',  TaskController.deleteProduct)
 
-module.exports = router
+// module.exports = router
