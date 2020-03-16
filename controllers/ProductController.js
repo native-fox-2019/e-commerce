37247f6efd0ab68b6d4c851ad5dd923d2aff65a0
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2020-03-16 13:19:30 +0700

    mindahin ke folder server

--- controllers/ProductController.js
+++
@@ -1,9 +0,0 @@
-const {Product}=require('../models');
-
-class ProductController{
-    static async create(req,res){
-        let {}
-    }
-}
-
-module.exports=ProductController;
--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 const route=require('./route');
 
-app.use(express.session())
+app.use(express.json())
 app.use(cors());
 
 app.use(route);
