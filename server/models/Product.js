8ecc1e48e3471f63e30751c970f736e8b8454ae0
lifegrain <terrykho@ymail.com>
2020-03-17 09:34:39 +0700

    test formatter

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,44 +1,48 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Product = sequelize.define('Product', {
-    name: {
-      type: DataTypes.STRING,
-      validate: {
-        notEmpty: {
-          msg: `Product Name can't be empty!`
+  const Product = sequelize.define(
+    "Product",
+    {
+      name: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: {
+            msg: `Product Name can't be empty!`
+          }
+        }
+      },
+      image_url: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: {
+            msg: `Image url can't be empty!`
+          }
+        }
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        validate: {
+          min: {
+            args: 1,
+            msg: `Price must be at least 1`
+          }
+        }
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        validate: {
+          min: {
+            args: 1,
+            msg: `Stock must be at least 1`
+          }
         }
       }
     },
-    image_url: {
-      type: DataTypes.STRING,
-      validate: {
-        notEmpty: {
-          msg: `Image url can't be empty!`
-        }
-      }
-    },
-    price: {
-      type: DataTypes.INTEGER,
-      validate: {
-        min: {
-          args: 1,
-          msg: `Price must be at least 1`
-        }
-      }
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      validate: {
-        min: {
-          args: 1,
-          msg: `Stock must be at least 1`
-        }
-      }
-    }
-  }, {});
-  Product.associate = function (models) {
+    {}
+  );
+  Product.associate = function(models) {
     // associations can be defined here
-    Product.belongsTo(models.User)
+    Product.belongsTo(models.User);
   };
   return Product;
 };
