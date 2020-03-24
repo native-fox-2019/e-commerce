c64478b6c91e2ac5490e690734f72669a7dd8d56
Malik ibrahim Nur <222malikibrahim@gmail.com>
2020-03-24 22:33:03 +0700

    add endpoint login and get usertoken

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,4 +1,6 @@
 const { User, Cart } = require('../models')
+const { compare } = require('../helpers/bcrypt')
+const jwt = require('jsonwebtoken')
 
 class Controller {
   static register(req, res, next) {
@@ -31,8 +33,40 @@ class Controller {
   }
 
   static login(req, res, next) {
-
+    const { email, password } = req.body
+    let id, first_name, last_name
+    User.findOne({
+      where: { email }
+    })
+     .then(user => {
+       if(user){
+        id = user.id
+        first_name = user.first_name
+        last_name = user.last_name     
+        return compare(password, user.password)
+       }else{
+         next({status: 400, message:'Email not registered'})
+       }
+     })
+     .then(result => {
+       if(result){
+        let token = jwt.sign({
+          id,
+          first_name,
+          last_name,
+          email
+        }, process.env.SECRET)
+        res.status(200).json({usertoken: token})
+       }else{
+         next({status: 401, message:'Wrong password!'})
+       }
+     })
+     .catch(err => {
+       next(err)
+     })
   }
+
+
 }
 
 module.exports = Controller
