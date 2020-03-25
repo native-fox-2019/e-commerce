2bc44d5059bd0eea522cf36d0f5add855d4c962e
adhiyatmarama <adhiyatma.pramayoga@gmail.com>
2020-03-25 09:45:11 +0700

    Skeleton awal client dan membuat header dan footer

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="footer">
+        <span>© 2020 Adhiyatma Pramayoga.</span>
+    </div>
+</template>
+<script>
+export default {
+};
+</script>
---
+++ client/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="header">
+        <div class="header-content">
+            <div class="title">
+                <span>Footsalt.com</span>
+            </div>
+            <div class="nav">
+                <router-link to="/">Home</router-link>
+                <router-link to="/login">Sign In</router-link>
+                <router-link to="/register">Register</router-link>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+};
+</script>
