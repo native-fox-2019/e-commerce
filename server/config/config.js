fcfd5c452251731b093b54c6eb2ce01bb7434402
akbarakma <akbarajo1234@gmail.com>
2020-03-26 01:19:09 +0700

    Menambahkan Procfile dan menggatin production credential

--- server/config/config.js
+++ server/config/config.js
@@ -16,11 +16,10 @@ module.exports = {
     "operatorsAliases": false
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+    "username": "rllfpswvdgmpwi",
+    "password": "f8b7737a24719a4cf1c6499a960e5122e666983da6a9ee57b00f466ff3e6d8a0",
+    "database": "db2lq48ud64977",
+    "host": "ec2-3-211-48-92.compute-1.amazonaws.com",
+    "dialect": "postgrest"
   }
 }
