120445c54d26cc92979c2ce887e57b19761c7971
umbone <henarivanaagit@gmail.com>
2020-03-26 09:46:29 +0700

    ganti config untuk deploy

--- server/config/config.js
+++ server/config/config.js
@@ -15,11 +15,10 @@ module.exports = {
     "operatorsAliases": false
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+    "username": "echmjisdrorvms",
+    "password": "df358613fa4c2d4bd9a96f8b3287d98256ac7a68b017a8d456dbd61a4fbb261c",
+    "database": "d9ge8vqq08a72t",
+    "host": "ec2-18-235-20-228.compute-1.amazonaws.com",
+    "dialect": "postgres"
   }
 }
