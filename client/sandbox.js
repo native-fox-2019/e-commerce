f5db11ed0c89dae1c1501b78cc74d37cc291a11c
agunggst <rqz.agastya@gmail.com>
2020-03-26 11:12:32 +0700

    nambah git ignore hapus file sandbox

--- client/sandbox.js
+++
@@ -1,6 +0,0 @@
-let date = new Date()
-const dayName = ['Sun', 'Mon', 'Tue', 'Wed', 'Thurs', 'Fri', 'Sat']
-
-console.log(`${dayName[date.getDay()]}, ${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`)
-console.log(`${dayName[date.getDay()]}, ${date.getDate()}`)
-console.log(`${date.getHours()}:${date.getMinutes()}`)
