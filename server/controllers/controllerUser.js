845b7943863f0847e97a2259dc1d7cc4b3e70415
errysatrio <erry.satrio@yahoo.co.id>
2020-03-26 10:03:40 +0700

    deploy server

--- server/controllers/controllerUser.js
+++ server/controllers/controllerUser.js
@@ -1,7 +1,7 @@
 'use strict'
 
 require('dotenv').config()
-const {Sequelize,User, Product, Cart } = require('../models')
+const { Sequelize, User, Product, Cart } = require('../models')
 const { sign } = require('../helpers/jwt')
 const { checkPass } = require('../helpers/bcrypt')
 const Op = Sequelize.Op
@@ -20,36 +20,37 @@ class ControllerUser {
         Cart.findOne({
             where: {
                 [Op.and]: [
-                    {UserId: req.user.id},
-                    {ProductId: req.params.id}
+                    { UserId: req.user.id },
+                    { ProductId: req.params.id }
                 ]
             }
         })
-        .then(data =>{
-            if(data){
-                let payload = {
-                    UserId: data.UserId,
-                    ProductId: data.ProductId,
-                    amount: data.amount+Number(req.body.amount)
+            .then(data => {
+                if (data) {
+                    let payload = {
+                        UserId: data.UserId,
+                        ProductId: data.ProductId,
+                        amount: data.amount + Number(req.body.amount)
+                    }
+                    return Cart.update(payload, { where: { id: data.id } })
+                } else {
+                    let payload = {
+                        UserId: req.user.id,
+                        ProductId: Number(req.params.id),
+                        amount: req.body.amount
+                    }
+                    console.log(payload)
+                    return Cart.create(payload)
                 }
-                return Cart.update(payload, {where:{id:data.id}} )
-            } else {
-                let payload = {
-                    UserId: req.user.id,
-                    ProductId: Number(req.params.id),
-                    amount: req.body.amount
-                }
-                console.log(payload)
-                return Cart.create(payload)
-            }
-        })
+            })
             .then(data => res.status(201).json({ message: `item(s) has been added to your cart` }))
             .catch(err => {
                 console.log(err)
-                next(err)})
-        }
+                next(err)
+            })
+    }
     static cart(req, res, next) {
-        User.findAll({where:{id:req.user.id}, include: [Cart]})
+        User.findAll({ where: { id: req.user.id }, include: [Cart] })
             .then(data => {
                 res.status(200).json(data)
             })
@@ -77,19 +78,19 @@ class ControllerUser {
     }
     static checkout(req, res, next) {
         let objUpdate = null
-        Cart.findAll({ where: {UserId: req.user.id}, include: [Product]})
-        .then(data =>{
-            objUpdate = data
-            return Product.bulkCreate(data.Product, { updateOnDuplicate: ['stocks', 'updatedAt'] })
-        })
-        .then(data => Cart.destroy({ where: {UserId: req.user.id}}))
-        .then(data => res.status(200).json({
-            message: 'succesfully check out'
-        }))
-        .catch(err => {
-            console.log(err)
-            next(err)
-        })
+        Cart.findAll({ where: { UserId: req.user.id }, include: [Product] })
+            .then(data => {
+                objUpdate = data
+                return Product.bulkCreate(data.Product, { updateOnDuplicate: ['stocks', 'updatedAt'] })
+            })
+            .then(data => Cart.destroy({ where: { UserId: req.user.id } }))
+            .then(data => res.status(200).json({
+                message: 'succesfully check out'
+            }))
+            .catch(err => {
+                console.log(err)
+                next(err)
+            })
     }
 }
 
