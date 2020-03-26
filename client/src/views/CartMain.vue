1fd7cde8571f0ba28eb638288f93ed090e30d565
Singgit Bramantha <bambadom@gmail.com>
2020-03-26 11:09:15 +0700

    deploy

--- client/src/views/CartMain.vue
+++ client/src/views/CartMain.vue
@@ -3,8 +3,7 @@
     <div class="mb-3"><h3><u>My Cart</u></h3></div>
     <div class="d-flex-justify-content-space-between flex-warp">
      <b-button pill variant="success" @click.prevent="Logout" class="float-right mr-5">Logout</b-button>
-            <b-button pill variant="primary" @click.prevent="checkout">checkout</b-button>
-    <b-button pill variant="warning" @click.prevent="$router.push('/')" class="float-left ml-5">Back</b-button></div>
+    <b-button pill variant="warning" @click.prevent="$router.push('/userPage')" class="float-left ml-5">Back</b-button></div>
   <div></div>
     <div class="mt-5">
     <div class="row ml-auto mr-auto">
