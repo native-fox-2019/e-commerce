bca36ab5a217b2291452f5d8179fb7f67565f356
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2021-06-17 15:11:06 +0700

    check connection add

---
+++ server/check-connection.js
@@ -0,0 +1,19 @@
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/config/config.json')[env];
+const Sequelize = require('sequelize');
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
