7b72f2864d6625d6fc479a18384ed3822f75c990
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2021-06-17 16:32:25 +0700

    change check-connecton directly to pg

--- server/check-connection.js
+++ server/check-connection.js
@@ -1,30 +1,18 @@
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/config/config.json')[env];
 const Sequelize = require('sequelize');
+const { Client } = require('pg')
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-console.log('Using:'+env, { config });
-
-const { Product } = require('./models');
-
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
-
-Product.findAll().then(products => {
-  console.log("All Products:", JSON.stringify(products.slice(0,5), null, 4));
+const client = new Client({
+  user: config.username,
+  host: config.host,
+  database: config.database,
+  password: config.password,
+  port: config.port,
+})
+client.connect().then(() => {
+  console.log('BERHIASIL CONNECT')
 })
 .catch((err) => {
-  console.log('error on load product data',err);
-});
+  console.log('Gagal untuk connect',err);
+})
